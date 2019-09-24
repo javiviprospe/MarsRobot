31b908fab8b9e237ad2f1f17ddaab7688e7816b0
javiviprospe <javier.gomez.palacios@gmai.com>
2019-09-24 12:50:43 +0200

    Adapted test to scent

--- src/test/java/functionalWhole.java
+++ src/test/java/functionalWhole.java
@@ -1,3 +1,6 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 import utils.Robot;
 
@@ -10,6 +13,9 @@ public class functionalWhole {
             .moveOneStep().changeOrientation("R").moveOneStep().changeOrientation("R").moveOneStep().changeOrientation("R").moveOneStep();
 
         System.out.println("Moved "+robot.getPosition()+" Lost? "+robot.isOutOfGrid()+" Position "+robot.getOrientation());
+        assertEquals(robot.getPosition(),"1 1");
+        assertEquals(robot.isOutOfGrid(),false);
+        assertEquals(robot.getOrientation(),"E");
     }
     @Test
     public void setRobot2(){
@@ -21,15 +27,20 @@ public class functionalWhole {
 
 
         System.out.println("Moved "+robot.getPosition()+" Lost? "+robot.isOutOfGrid()+" Position "+robot.getOrientation());
+        assertEquals(robot.getPosition(),"3 3");
+        assertEquals(robot.isOutOfGrid(),true);
+        assertEquals(robot.getOrientation(),"N");
     }
     @Test
     public void setRobot3(){
         Robot robot = new Robot();
-        robot.setPositionBoundaries(5,3).setPositionCurrent(0,3).setOrientation("W").changeOrientation("L")
+        robot.setPositionBoundaries(5,3).setPositionCurrent(0,3).setPositionScent(2,3).setOrientation("W").changeOrientation("L")
             .changeOrientation("L").moveOneStep().moveOneStep().moveOneStep().changeOrientation("L").moveOneStep()
             .changeOrientation("L").moveOneStep().changeOrientation("L");
         System.out.println("Moved "+robot.getPosition()+" Lost? "+robot.isOutOfGrid()+" Position "+robot.getOrientation());
-
+        assertEquals(robot.getPosition(),"2 3");
+        assertEquals(robot.isOutOfGrid(),false);
+        assertEquals(robot.getOrientation(),"S");
     }
 
 
