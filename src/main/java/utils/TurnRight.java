18a68b9eac08132424bcf96a7f99aab0c85d0d0c
javiviprospe <javier.gomez.palacios@gmai.com>
2019-09-24 11:19:09 +0200

    first files. First interfaces to interact with. added classes which interact with those interfacefirst files. First interfaces to interact with. added classes which interact with those interfacess

---
+++ src/main/java/utils/TurnRight.java
@@ -0,0 +1,21 @@
+package utils;
+
+import utils.interfaces.InteractOrientation;
+
+public class TurnRight implements InteractOrientation<Position> {
+
+    public String performOrientation(Position p) {
+        String newOrientation = new String("");
+        if (p.getOrientation().equals("S"))
+            newOrientation = "W";
+        else if (p.getOrientation().equals("N"))
+            newOrientation = "E";
+        else if (p.getOrientation().equals("W"))
+            newOrientation = "N";
+        else if (p.getOrientation().equals("E"))
+            newOrientation = "S";
+        else
+            newOrientation = "";
+        return newOrientation;
+    }
+}
